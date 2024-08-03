862a2427f81bc80a4b6f7a55e1863a8e490e2f04
Yurii Diachuk <yurdia817@gmail.com>
2024-08-03 11:04:09 +0200

    added close accaunt function

--- script.js
+++ script.js
@@ -310,3 +310,28 @@ btnLoan.addEventListener('click', function (e) {
   clearInterval(timer);
   timer = startLogOutTimer();
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+});
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
